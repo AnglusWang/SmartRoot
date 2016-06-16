cb91bee2445ed1fa069d6a28d9418318f5b4eea0
AnglusWang <AnglusWang@163.com>
2016-06-16 21:19:07 +0800

    添加ChatMessage类用于存储信息

---
+++ app/src/main/java/com/angluswang/smartrobot/bean/ChatMessage.java
@@ -0,0 +1,51 @@
+package com.angluswang.smartrobot.bean;
+
+import java.util.Date;
+
+/**
+ * Created by Jeson on 2016/6/16.
+ * 聊天信息类
+ */
+
+public class ChatMessage {
+    private String mName;
+    private String mMsg;
+    private Type mType;
+    private Date mDate;
+
+    private enum Type {
+        INCOMING, OUTCOMING
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Type getType() {
+        return mType;
+    }
+
+    public void setType(Type type) {
+        mType = type;
+    }
+
+    public String getMsg() {
+        return mMsg;
+    }
+
+    public void setMsg(String msg) {
+        mMsg = msg;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+}
--- app/src/test/java/com/angluswang/smartrobot/ExampleUnitTest.java
+++ app/src/test/java/com/angluswang/smartrobot/ExampleUnitTest.java
@@ -1,7 +1,5 @@
 package com.angluswang.smartrobot;
 
-import com.angluswang.smartrobot.utils.HttpUtils;
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -15,16 +13,5 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
-
-        String res = HttpUtils.doGet("能给我讲个笑话么");
-        System.out.println(res);
-        res = HttpUtils.doGet("笑话一点也不好听");
-        System.out.println(res);
-        res = HttpUtils.doGet("呵呵");
-        System.out.println(res);
-        res = HttpUtils.doGet("你叫什么");
-        System.out.println(res);
-        res = HttpUtils.doGet("做什么好呢");
-        System.out.println(res);
     }
 }
